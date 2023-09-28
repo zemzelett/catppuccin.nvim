4abddf148a20e843ac8b219a0c2e1484abc49c01
zemzelett <zemzelett@minoklu.com>
2023-09-28 23:45:55 +0200

    feat: change highlight names

--- lua/catppuccin/groups/integrations/indent_blankline.lua
+++ lua/catppuccin/groups/integrations/indent_blankline.lua
@@ -1,22 +1,15 @@
 local M = {}
 
 function M.get()
-	local hi = {
-		IndentBlanklineChar = { fg = C.surface0 },
-		IndentBlanklineContextChar = { fg = C.text },
-		IndentBlanklineContextStart = { sp = C.text, style = { "underline" } },
+	return {
+		RainbowDelimiterRed = { fg = C.red },
+		RainbowDelimiterYellow = { fg = C.yellow },
+		RainbowDelimiterBlue = { fg = C.blue },
+		RainbowDelimiterOrange = { fg = C.peach },
+		RainbowDelimiterGreen = { fg = C.green },
+		RainbowDelimiterViolet = { fg = C.mauve },
+		RainbowDelimiterCyan = { fg = C.teal },
 	}
-
-	if O.integrations.indent_blankline.colored_indent_levels then
-		hi["IndentBlanklineIndent6"] = { blend = 0, fg = C.yellow }
-		hi["IndentBlanklineIndent5"] = { blend = 0, fg = C.red }
-		hi["IndentBlanklineIndent4"] = { blend = 0, fg = C.teal }
-		hi["IndentBlanklineIndent3"] = { blend = 0, fg = C.peach }
-		hi["IndentBlanklineIndent2"] = { blend = 0, fg = C.blue }
-		hi["IndentBlanklineIndent1"] = { blend = 0, fg = C.pink }
-	end
-
-	return hi
 end
 
 return M
